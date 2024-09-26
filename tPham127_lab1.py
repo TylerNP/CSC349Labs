d923dc206c99c7fc79875ffd8d7525aa2d3c4cb1
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-09-25 23:24:38 -0700

    Removed extra text

--- tPham127_lab1.py
+++ tPham127_lab1.py
@@ -1,24 +1,13 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("ERROR: Include a filename")
-    exit()
 filename = sys.argv[1]
-try:
-    file = open(filename, 'r')
-except:
-    print("ERROR: File ({}) not found in directory".format(filename))
-    exit()
+file = open(filename, 'r')
 lines = file.readlines()
 
 found = False  
 low = 0
 high = len(lines)-1 
 middle = high//2
-
-if high == -1:
-    print("ERROR: Empty list")
-    exit()
 
 if middle == 0:
     print(lines[middle].strip())
