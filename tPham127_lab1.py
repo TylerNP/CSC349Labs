5386c3fdd97bf9b715d12a2ae11038c631abfa8f
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-09-28 17:18:12 -0700

    algorithm

--- tPham127_lab1.py
+++ tPham127_lab1.py
@@ -8,20 +8,33 @@ found = False
 low = 0
 high = len(lines)-1 
 middle = high//2
-
 if middle == 0:
     print(lines[middle].strip())
     found = True
 
-while not found:
+step = 0
 
+while not found:
+    side_checked = 1
+    adjustment = 0
+    #determine if middle is the first or second in the pair if it is not the singleton
+    if middle % 2 == 0:
+        side_checked = -1
+        adjustment = 1
+
+    step += 1
+    print(f"STEP {step}: |{low}|{middle}|{high}|{lines[middle]}")
     value = int(lines[middle])
-    if value == int(lines[middle-1]):
-        low = middle+1
-        middle = low-1+(high-low)//2
-    elif value == int(lines[middle+1]):
-        high = middle-1
-        middle = low+1+(high-low)//2
+    if middle == 0 or middle == len(lines)-1 or low == high:
+        found = True
+        print(value)
+        break
+    if value == int(lines[middle-side_checked]):
+        low = middle+1+adjustment
+        middle = low+(high-low)//2
+    elif value == int(lines[middle+side_checked]):
+        high = middle-1-adjustment
+        middle = low+(high-low)//2
     else:
         print(value)
         found = True
