3f740705c254465987368aa42312eb5f967600b4
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-10-15 15:57:52 -0700

    added template for lab2

--- majority.py
+++ majority.py
@@ -9,25 +9,24 @@ def majority(s_1 : list[int], s_2 : list[int]) -> list[int]:
         return s_2
     if not s_2:
         return s_1
-    if s_1[-1] == s_2[-1]:
-        s_1.append(s_2[-1])
-        s_2.pop(0)
+    if s_1[0] > s_2[0]:
         return majority(s_1, s_2)
     else:
-        if len(s_1) > len(s_2):
-            return []
-        else:
-            return []
+        return 
 
 def majority_search(A : list[int]) -> list[int]:
     if not A:
         return []
     if len(A) == 1:
-        print("RAN")
-        return A
+        return A[0]
+    if len(A) == 2:
+        if A[0] == A[1]:
+            return A[0]
+        else:
+            return []
     middle = (len(A)-1)//2
-    s_1 = majority_search(A[0 : middle : 1])
-    s_2 = majority_search(A[middle+1 : len(A)-1 : 1])
-    return majority(s_1, s_2)
+    s_1 = majority_search(A[0 : middle+1 : 1])
+    s_2 = majority_search(A[middle+1 : len(A) : 1])
+    return majority(s_1, s_2)[middle]
 
 print(majority_search(lines))
