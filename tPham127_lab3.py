eaa61b4242f9dca1471321a0dcba540ccf25cbbe
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-11-09 15:05:24 -0800

    implemented levenchstein distance

--- tPham127_lab3.py
+++ tPham127_lab3.py
@@ -1,23 +1,14 @@
 import sys
 
-#word_1 = sys.argv[1]
-#word_2 = sys.argv[2]
-
 def edit_distance(word_1 : str, word_2 : str) -> None:
-    matrix = [[0 for _ in range(len(word_1)+1)] for _ in range(len(word_2)+1)]
-    for index in range(len(word_2)+1):
-        matrix[index][0] = index
-    for index in range(len(word_1)+1):
-        matrix[0][index] = index
-    for m in range(len(word_2)+1):
-        for n in range(len(word_1)+1):
-            print()
-    print(matrix)
-    print(word_1 + " " + word_2)
-
-#edit_distance(word_1, word_2)
+    matrix = [[y + x if y == 0 or x == 0 else 0 for x in range(len(word_1)+1)] for y in range(len(word_2)+1)]
+    for m in range(len(word_2)):
+        for n in range(len(word_1)):
+            add = 0 if word_1[n] == word_2[m] else 1
+            matrix[m+1][n+1] = max(min(matrix[m][n], matrix[m][n+1], matrix[m+1][n]) + add, m-n)
+    return matrix[len(word_2)][len(word_1)]
 
 if __name__ == "__main__":
-    str_1 = "SNOWY"
-    str_2 = "SUNNY"
-    edit_distance(str_1, str_2)
+    word_1 = sys.argv[1]
+    word_2 = sys.argv[2]
+    print(edit_distance(word_1, word_2))
