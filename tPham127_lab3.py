c037299166ef2eab21509182a1bdc9ca40b5458e
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-11-08 20:30:13 -0800

    test

--- tPham127_lab3.py
+++ tPham127_lab3.py
@@ -5,6 +5,13 @@ import sys
 
 def edit_distance(word_1 : str, word_2 : str) -> None:
     matrix = [[0 for _ in range(len(word_1)+1)] for _ in range(len(word_2)+1)]
+    for index in range(len(word_2)+1):
+        matrix[index][0] = index
+    for index in range(len(word_1)+1):
+        matrix[0][index] = index
+    for m in range(len(word_2)+1):
+        for n in range(len(word_1)+1):
+            print()
     print(matrix)
     print(word_1 + " " + word_2)
 
